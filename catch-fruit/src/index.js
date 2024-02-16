a27f1af5868f4f212d50844ffa9d27afb998efad
João Vitor de Aquino <jvaquinocontact@gmail.com>
2024-02-16 08:54:12 -0300

    + start creating collision and fall logic

--- catch-fruit/src/index.js
+++ catch-fruit/src/index.js
@@ -1,14 +1,28 @@
 const Game = (() => {
   let lives, score;
+  let fruits, bombs, bonus;
 
   const start = () => {
     lives = 3;
     score = 0;
+    fruits = [], bombs = [], bonus = [];
 
   }
 
-  return {
+  const checkCollision = () => {
 
+  }
+
+  function isColliding(rect1, rect2) {
+    return rect1.x < rect2.x + rect2.width &&
+           rect1.x + rect1.width > rect2.x &&
+           rect1.y < rect2.y + rect2.height &&
+           rect1.y + rect1.height > rect2.y;
+  }
+
+
+  return {
+    start
   }
 })();
 
@@ -29,6 +43,7 @@ const Board = (() => {
 })();
 
 const Basket = (() => {
+  const basketUI = document.querySelector("#Basket")
 
   const move = (() => {
     
@@ -39,15 +54,17 @@ const Basket = (() => {
   }
 })();
 
-const Fruit = () => {
-
+function Fruit() {
+  const startX = Math.floor(Math.random() * 60 + 20);
+  
   return {
 
   }
 }
 
-const Bomb = () => {
-
+function Bomb() {
+  const startX = Math.floor(Math.random() * 60 + 20);
+  
   return {
 
   }
